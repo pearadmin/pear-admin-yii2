6488d04149b8b5a6e4cad8da1caa0e7aee89b9b2
earnest <464605059@qq.com>
2021-07-08 23:34:05 +0800

    修复local文件缺失问题

---
+++ backend/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'r6XcR9-5qNKHYS19986M5uuhsLQMESNH',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+   /* $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];*/
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
