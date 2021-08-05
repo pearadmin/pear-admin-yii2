323dc90662eb4e1c59fd82f204122b8bb81d6950
earnest <464605059@qq.com>
2021-08-05 21:05:10 +0800

    登录模块新增滑块验证

--- backend/modules/rbac/views/user/js/login.js
+++ backend/modules/rbac/views/user/js/login.js
@@ -29,15 +29,4 @@ layui.config({
         }
         return false;
     });
-
-
-    //监听提交
-    form.on('submit(formDemo)', function(data) {
-        if(slider.isOk()){//用于表单验证是否已经滑动成功
-            layer.msg(JSON.stringify(data.field));
-        }else{
-            layer.msg("请先通过滑块验证");
-        }
-        return false;
-    });
 })
