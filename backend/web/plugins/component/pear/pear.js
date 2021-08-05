4626d25203645e191fbad99d460688376e7575a5
earnest <464605059@qq.com>
2021-08-05 21:01:01 +0800

    滑块组件加入pear.js

--- backend/web/plugins/component/pear/pear.js
+++ backend/web/plugins/component/pear/pear.js
@@ -46,7 +46,8 @@ layui.config({
 	http: "http",			// ajax请求组件
 	theme: "theme",			// 主题转换
 	ajax: "ajax",			// ajax请求组件
-	gatewayWorker: "gatewayWorker"	// gatewayWorker组件
+	gatewayWorker: "gatewayWorker",	// gatewayWorker组件
+	slierVerify: "slierVerify"	// gatewayWorker组件
 }).use(['layer', 'theme'], function () {
 	layui.theme.changeTheme(window, false);
 });
