225898fadcc86f584fc6e0c8f964fb191d4d3635
earnest <464605059@qq.com>
2021-09-02 23:32:27 +0800

    up:新增redis和queue组件

--- common/config/main.php
+++ common/config/main.php
@@ -4,10 +4,26 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'bootstrap' => [
+        'queue',
+        'redis'
+    ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ]
+        ],
+        'queue'  => [
+            'class'=>\yii\queue\redis\Queue::class,
+            'redis'   => 'redis',
+            'channel' => 'queue',
+            'as log'=> \yii\queue\LogBehavior::class,
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => '127.0.0.1',
+            'port' => 6379,
+            'database' => 0,
+        ],
     ],
 ];
